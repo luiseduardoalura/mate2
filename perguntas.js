b1aa0d9ea41ca7c8bba007c1f8ba0c83566b2ebd
luiseduardoalura <105978532+luiseduardoalura@users.noreply.github.com>
2024-10-02 19:21:11 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'Programação',
+    'O que é uma variável em programação??',
+    'Qual é a diferença entre const e let no JavaScript?' ,
+)
+
+criaCartao(
+    'Português',
+    'O que é um sujeito em uma frase?',
+    'Qual é a diferença entre conotação e denotação?' ,
+)
+
+criaCartao(
+    'Programação',
+    'O que são funções recursivas? Quando você as utilizaria?',
+    'Como funciona o conceito de closures no JavaScript?' ,
+)
+
+criaCartao(
+    'Mtematica',
+    'Qual é o valor de π (pi) aproximado? ' ,
+    'Qual é o valor de π (pi) aproximado? ' ,
+)
+
+criaCartao(
+    'Química',
+    'Qual é a fórmula química da água?',
+    'O que é um átomo?' ,
+)
